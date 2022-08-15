e41f6ee43150ea27495033cca93090479b83f55e
amir <a.moharami@gmail.com>
2022-08-15 15:28:56 +0430

    add phpunit and set configuration

---
+++ tests/cqrsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace sisiun\cqrs\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class cqrsTest extends TestCase
+{
+
+    public function test_it_return_true()
+    {
+        $this->assertTrue(true);
+        $this->assertTrue(true);
+    }
+
+}
