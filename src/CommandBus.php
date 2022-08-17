ebdbea8264279ae54c0f6e2fa8b1b11e7428ef98
amir <a.moharami@gmail.com>
2022-08-17 19:01:37 +0430

    fix handle return type

--- src/CommandBus.php
+++ src/CommandBus.php
@@ -10,12 +10,12 @@ class CommandBus
     /**
      * @throws ReflectionException
      */
-    public function handle($object , $command): array
+    public function handle($object , $command): void
     {
         $reflection = new ReflectionClass($command);
         $handlerName = str_replace('Command', 'Handler', $reflection->getShortName());
         $handlerName = str_replace($reflection->getShortName(), $handlerName, $reflection->getName());
         $handler = new ($handlerName);
-        return $handler($object, $command);
+        $handler($object, $command);
     }
 }
